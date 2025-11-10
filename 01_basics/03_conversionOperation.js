76bd41b09191e24e293a02ee55d9ccadacf2c760
shubhamShreeAgrawal <shubham@getambee.com>
2025-11-10 09:36:48 +0530

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,3 +30,47 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 // number is converted to string here
+
+// *********************** Operations ***********************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue); -- -3
+
+// console.log(2+2); 
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);  -- 12
+// console.log(1 + "2");  --12
+// console.log("1" + 2 + 2); -- 122
+// console.log(1 + 2 + "2");  -- 32
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);  -- 1
+// console.log(+"");  -- 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// prefix and postix operator
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
